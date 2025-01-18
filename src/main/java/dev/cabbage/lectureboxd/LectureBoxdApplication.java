b4af680f1648e87de3bcaac9e73dce995c96bdb6
walidmenou <walid.menou@outlook.com>
2025-01-18 14:33:37 -0500

    Initial commit

---
+++ src/main/java/dev/cabbage/lectureboxd/LectureBoxdApplication.java
@@ -0,0 +1,13 @@
+package dev.cabbage.lectureboxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureBoxdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureBoxdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cabbage/lectureboxd/LectureBoxdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cabbage.lectureboxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureBoxdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
